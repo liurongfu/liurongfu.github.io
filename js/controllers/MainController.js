174ac9c674a0ee38d53ee57f14adffb6fda9e889
liurongfu <liurongfu123@gmail.com>
2016-11-21 11:59:38 +0800

    test

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,30 +1,10 @@
-
 var baseUrl = "http://110.86.16.43:30009";
-
 var getCurrentUserNameUrl = baseUrl + "/user/currentUserName";
-app.controller("MainController", function ($scope, $location, $http, $modal, $timeout) {
-
+app.controller("MainController", function ($scope, $http) {
     $scope.userName;
-
-    // 警告框模块
-    $scope.alerts = [];
-    $scope.addAlert = function (type, message) {
-        $scope.alerts.push({type: type, msg: message});
-        $scope.autoHide();
-    };
-    $scope.closeAlert = function () {
-        $scope.alerts.pop();
-    };
-    $scope.autoHide = function () {
-        $timeout(function () {
-            $scope.closeAlert();
-        }, 5000);
-    };
-
     // 页面加载
     load();
     function load() {
-	alert(getCurrentUserNameUrl);
         // 取得当前用户名
         $http.get(getCurrentUserNameUrl).success(function (userName) {
             $scope.userName = userName;
